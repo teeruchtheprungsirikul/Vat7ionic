fa48b48597b386d49c58e74c02eafdf50c737958
teeruchtheprungsirikul <teeruchtheprungsirikul@MacBook-Pro-khxng-Teeruch.local>
2021-07-03 15:46:38 -0700

    some soft of commit message

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  vatResult = 0;
+
+  calculate(money) {
+    // alert('OK ' + money);
+
+    let result = money * 107/100;
+    console.log(result);
+    this.vatResult = result;
+  }
+
+}
